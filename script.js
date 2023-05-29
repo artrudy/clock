6df9ef1cc682b9f0c1a4f19ea73fe48a7e24ba07
artrudy <art.rudy@gmail.com>
2023-05-29 22:29:30 +0300

    project created

---
+++ script.js
@@ -0,0 +1,14 @@
+function clock(params) {
+    const now = new Date();
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    //Setup canvas
+
+    ctx.save(); //save the default state
+
+
+    ctx.restore(); // restore default state
+           
+}
+clock();
